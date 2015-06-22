3d16d1bced3b11d0fdcdae2ef797ed8ed408519d
AmritDeep <amritdeepdhungana@hotmail.com>
2015-06-23 01:13:20 +0545

    if output file exist the move it to older file and crete new file

--- ruby_scrapper.rb
+++ ruby_scrapper.rb
@@ -1,17 +1,29 @@
 require "nokogiri"
 require "open-uri"
-require "pry"
+require "fileutils"
+require "time"
 
+private
+def create_file(file, variable)
+	out_file = File.new(file, "w")
+	out_file.puts(variable)
+	out_file.close	
+end
 
+pwd = Dir.pwd
 doc = Nokogiri::HTML(open("http://www.indeed.com/"))
 tx = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=software+engineer&l=Texas"))
-
-# css = tx.css("div.row.result").map do |tx|
-# 	 data = tx.text.strip
-# end
 css = tx.css("div.row.result").collect {|node| node.text.strip}
 
-out_file = File.new("output.txt", "w")
-out_file.puts(css)
-out_file.close
+if File.exist?('output.txt')
+	# FileUtils.mkdir_p 'Data'
+	FileUtils.mv("#{pwd}/output.txt", "#{pwd}/#{Time.now.strftime("%Y%M%d%H%M")}_output.txt")
+	create_file('output.txt', css)
+else
+	create_file('output.txt', css)
+end
+ 
+
+
+
 
