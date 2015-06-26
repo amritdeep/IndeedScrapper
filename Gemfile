8560b0b46eb0add4c8f6bfe61b66e7698d06487a
AmritDeep <amritdeepdhungana@hotmail.com>
2015-06-26 22:37:24 +0545

    check whether title and location contain space or not

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 gem 'mechanize'
 gem 'nokogiri', '~> 1.6.6.2'
+gem 'rspec', '~> 3.3.0'
--- ruby_scrapper.rb
+++ ruby_scrapper.rb
@@ -2,6 +2,7 @@ require "nokogiri"
 require "open-uri"
 require "fileutils"
 require "time"
+require "pry"
 
 private
 def create_file(file, variable)
@@ -10,18 +11,30 @@ def create_file(file, variable)
 	out_file.close	
 end
 
-# READ INPUT FROM COMMAND
+## Read Input from Command
+puts "Reading Data From Input"
 puts "What Job Do you Want ? (Job Title)"
 job_title = gets.chomp
 puts "Where Do you Want ? (Location)"
 location = gets.chomp
+# job_title="software developer"
+# location="Texas"
 
+puts "You are searching for #{job_title} in #{location}"
 
-pwd = Dir.pwd
+## Check title and location contain space or not
+job_title=job_title.sub(/ /, '+') if job_title.match(/\s/)
+location=location.sub(/ /, '+')  if location.match(/\s/)
+
+puts "JOB : #{job_title} || LOC : #{location}"
+
+## Take input in indeed site to enter the value
 doc = Nokogiri::HTML(open("http://www.indeed.com/"))
-tx = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=software+engineer&l=Texas"))
+tx = Nokogiri::HTML(open("http://www.indeed.com/jobs?q=#{job_title}&l=#{location}"))
 css = tx.css("div.row.result").collect {|node| node.text.strip}
 
+## Create file or move it
+pwd = Dir.pwd
 if File.exist?('output.txt')
 	# FileUtils.mkdir_p 'Data'
 	FileUtils.mv("#{pwd}/output.txt", "#{pwd}/#{Time.now.strftime("%Y%M%d%H%M")}_output.txt")
